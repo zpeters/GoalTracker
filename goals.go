4652887c4bc70bef6ee442734233eca26799c417
Zach Peters <zpeters@gmail.com>
2012-11-27 20:32:26 -0600

    gofmt cleaned up

--- goals.go
+++ goals.go
@@ -1,50 +1,50 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"io/ioutil"
-	"strings"
 	"regexp"
 	"strconv"
-	"html/template"
-	"bytes"
+	"strings"
 )
 
-const goalsPath	= "c:\\users\\zach\\Projects\\ZachCore\\Organizer\\TODO.org"
+const goalsPath = "c:\\users\\zach\\Projects\\ZachCore\\Organizer\\TODO.org"
 const fitnessPath = "c:\\users\\zach\\Projects\\ZachCore\\Organizer\\Fitness.org"
 const templatePath = "Template"
 
-const stretchesGoal	= true
-const walkRunGoal	= 1.0
-const squatsGoal	= 20
-const pushupsGoal	= 10
-const lungesGoal	= 20
-const rowsGoal		= 10
-const planksGoal	= 15
-const jumpingJacksGoal	= 30
-const weightGoal	= 190
+const stretchesGoal = true
+const walkRunGoal = 1.0
+const squatsGoal = 20
+const pushupsGoal = 10
+const lungesGoal = 20
+const rowsGoal = 10
+const planksGoal = 15
+const jumpingJacksGoal = 30
+const weightGoal = 190
 
 type Goal struct {
-	Name			 string
-	Description		 string
-	PercentComplete 	 float32
+	Name            string
+	Description     string
+	PercentComplete float32
 }
 
 type Workout struct {
-	Date		 string
-	Stretches	 bool
-	WalkRun		 float64
-	Squats		 int64
-	Pushups		 int64
-	Lunges		 int64
-	Rows		 int64
-	Planks		 int64
-	JumpingJacks	 int64
-	Weight		 float64 
+	Date         string
+	Stretches    bool
+	WalkRun      float64
+	Squats       int64
+	Pushups      int64
+	Lunges       int64
+	Rows         int64
+	Planks       int64
+	JumpingJacks int64
+	Weight       float64
 }
 
 type Page struct {
-	Goals []Goal
+	Goals    []Goal
 	Workouts []Workout
 }
 
@@ -62,22 +62,26 @@ func goalParser() []Goal {
 
 	// load the goals file
 	content, err := ioutil.ReadFile(goalsPath)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	lines := strings.Split(string(content), "\n")
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 
-	for _ ,line := range lines {
+	for _, line := range lines {
 		b := []byte(line)
 		if longTermRegex.Match(b) {
 			state = "longterm"
-		} else if epicRegex.Match(b){
+		} else if epicRegex.Match(b) {
 			state = "epic"
 		} else if goalRegex.Match(b) {
 			goalArray := strings.Split(line, "** ")
 			goalString := goalArray[1]
-			g := Goal {
-				Name: goalString,
-				Description: state,
+			g := Goal{
+				Name:            goalString,
+				Description:     state,
 				PercentComplete: 0.0,
 			}
 			goals = append(goals, g)
@@ -95,48 +99,70 @@ func workoutParser() []Workout {
 
 	// load the workouts file
 	content, err := ioutil.ReadFile(fitnessPath)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	lines := strings.Split(string(content), "\n")
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 
-	for _ ,line := range lines {
+	for _, line := range lines {
 		b := []byte(line)
 		if workoutRegex.Match(b) {
 			workoutArray := strings.Split(line, "|")
 
 			date := workoutArray[1]
-			stretches,err := strconv.ParseBool(strings.TrimSpace(workoutArray[2]))
-			if err != nil { panic(err) }
-			walkRun,err := strconv.ParseFloat(strings.TrimSpace(workoutArray[3]), 64)
-			if err != nil { panic(err) }
-			squats,err := strconv.ParseInt(strings.TrimSpace(workoutArray[4]), 10, 64)
-			if err != nil { panic(err) }
-			pushups,err := strconv.ParseInt(strings.TrimSpace(workoutArray[5]), 10, 64)
-			if err != nil { panic(err) }
-			lunges,err := strconv.ParseInt(strings.TrimSpace(workoutArray[6]), 10, 64)
-			if err != nil { panic(err) }
-			rows,err := strconv.ParseInt(strings.TrimSpace(workoutArray[7]), 10, 64)
-			if err != nil { panic(err) }
-			planks,err := strconv.ParseInt(strings.TrimSpace(workoutArray[8]), 10, 64)
-			if err != nil { panic(err) }
-			jumpingJacks,err := strconv.ParseInt(strings.TrimSpace(workoutArray[9]), 10, 64)
-			if err != nil { panic(err) }
-			weight,err := strconv.ParseFloat(strings.TrimSpace(workoutArray[10]), 64)
-			if err != nil { panic(err) }		
-
-			w := Workout {
-				Date: date,
-				Stretches: stretches,
-				WalkRun: walkRun,
-				Squats: squats,
-				Pushups: pushups,
-				Lunges: lunges,
-				Rows: rows,
-				Planks: planks,
-				JumpingJacks: jumpingJacks,
-				Weight: weight,		
+			stretches, err := strconv.ParseBool(strings.TrimSpace(workoutArray[2]))
+			if err != nil {
+				panic(err)
 			}
-			
+			walkRun, err := strconv.ParseFloat(strings.TrimSpace(workoutArray[3]), 64)
+			if err != nil {
+				panic(err)
+			}
+			squats, err := strconv.ParseInt(strings.TrimSpace(workoutArray[4]), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			pushups, err := strconv.ParseInt(strings.TrimSpace(workoutArray[5]), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			lunges, err := strconv.ParseInt(strings.TrimSpace(workoutArray[6]), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			rows, err := strconv.ParseInt(strings.TrimSpace(workoutArray[7]), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			planks, err := strconv.ParseInt(strings.TrimSpace(workoutArray[8]), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			jumpingJacks, err := strconv.ParseInt(strings.TrimSpace(workoutArray[9]), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			weight, err := strconv.ParseFloat(strings.TrimSpace(workoutArray[10]), 64)
+			if err != nil {
+				panic(err)
+			}
+
+			w := Workout{
+				Date:         date,
+				Stretches:    stretches,
+				WalkRun:      walkRun,
+				Squats:       squats,
+				Pushups:      pushups,
+				Lunges:       lunges,
+				Rows:         rows,
+				Planks:       planks,
+				JumpingJacks: jumpingJacks,
+				Weight:       weight,
+			}
+
 			workouts = append(workouts, w)
 		}
 	}
@@ -156,7 +182,7 @@ func main() {
 
 	var out bytes.Buffer
 	p := Page{
-		Goals: goals,
+		Goals:    goals,
 		Workouts: workouts,
 	}
 	err = t.Execute(&out, p)
